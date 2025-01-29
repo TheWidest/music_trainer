7f7276b7c277565c3e402ab60e10f74a3056a28a
TheWidest <amanin.seveneighths@gmail.com>
2025-01-29 06:04:51 +0300

    added bass

--- music_trainer.go
+++ music_trainer.go
@@ -7,14 +7,12 @@ import (
 	"os"
 	"log"
 	"strings"
+	"strconv"
 	"math"
 	"math/rand"
 )
-
-const frets = 22
-
 func printer(sheet [][]int) {
-	fmt.Print("\n")
+	fmt.Print("\n\n\n")
 	for i := 0; i < len(sheet[0]); i++ {
 		fmt.Print("—")
 		for j := 0; j < len(sheet); j++ {
@@ -26,9 +24,7 @@ func printer(sheet [][]int) {
 			default:
 				fmt.Print("—", sheet[j][i])
 			}
-			if j < len(sheet) - 1 {
-				fmt.Print("————")
-			}
+			fmt.Print("————")
 		}
 		fmt.Print("\n")
 	}
@@ -43,12 +39,8 @@ func flute_trainer() {
 	// }
 }
 
-func guitar_trainer() {
-	number_of_attacks := 10
-	sheet := make([][]int, number_of_attacks)
-	for i := range sheet {
-		sheet[i] = make([]int, 6)
-	}
+func bass_trainer() {
+	frets := 20
 	chords_set := []string{"y", "yes"}
 	no_chords_set := []string{"n", "no"}
 	var chords bool
@@ -72,13 +64,163 @@ func guitar_trainer() {
 		}
 	}
 
+	fmt.Println("How many attacks should there be in a set?")
+	var number_of_attacks int
+	for {
+		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		str = strings.TrimSpace(str)
+
+		if number_of_attacks_buf, err := strconv.Atoi(str); err == nil {
+			number_of_attacks = number_of_attacks_buf
+			break
+		} else {
+			fmt.Println("That's not a valid number, try again")
+		}
+	}
+	sheet := make([][]int, number_of_attacks)
+	for i := range sheet {
+		sheet[i] = make([]int, 4)
+	}
+
 	fmt.Println("Press 'Enter' to get a new set")
 	if chords {
-		anchor_fret := int(rand.Int31n(21))
+		anchor_fret := int(rand.Int31n(int32(frets - 1)))
+		for {
+			for k := 0; k < number_of_attacks; k++ {
+				barre := rand_bool(1, 6)
+				var third_finger int
+				if rand_bool(1, 4) {
+					third_finger = 1	// 1 if the chord'll use extra finger,
+				} else {
+					third_finger = 0	// otherwise 0
+				}
+				chord_buffer := make([]int, 4)
+				for i := 0; i < len(chord_buffer); i++{
+					chord_buffer[i] = 99
+				}
+				//99 - untouched cell
+				//-1 - skip 
+
+				for i := 0; i < 2 + third_finger; i++ { //'places fingers on strings'
+					for {
+						string_num := rand.Int31n(4)	//chooses the string for the finger
+						if chord_buffer[string_num] == 99 {	// checks if the string already 'has a finger on it'
+							var fret_modifier int
+							for {
+								fret_modifier = int(rand.Int31n(5))
+								if anchor_fret + fret_modifier <= frets { // checks if the fret is on the fretboard
+									break
+								}
+							}
+							chord_buffer[string_num] = anchor_fret + fret_modifier
+							break
+						}
+					}
+				}
+
+				if barre {
+					for i := 0; i < slices.Index(chord_buffer, slices.Min(chord_buffer)); i++ {
+						if chord_buffer[i] == 99 {
+							chord_buffer[i] = slices.Min(chord_buffer)
+						}
+					}
+				}
+				for i := 0; i < 4; i++ {
+					if chord_buffer[i] == 99 {
+						if rand_bool(1, 8) {
+							chord_buffer[i] = 0
+						} else {
+							chord_buffer[i] = -1
+						}
+					}
+				}
+				sheet[k] = chord_buffer
+				anchor_fret = int(math.Abs(float64((anchor_fret + int(rand.NormFloat64()*5)) % frets)))
+			}
+			printer(sheet)
+			_, err := bufio.NewReader(os.Stdin).ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		for {
+			fret := int(rand.Int31n(int32(frets + 1)))
+			for k := 0; k < number_of_attacks; k++ {
+				buff_attack := make([]int, 4)
+				for i := 0; i < 4; i++ {
+					buff_attack[i] = -1
+				}
+				fret = int(math.Abs(float64((fret + int(rand.NormFloat64()*9)) % frets)))
+				buff_attack[int(rand.Int31n(4))] = fret
+				sheet[k] = buff_attack
+			}
+
+			printer(sheet)
+			_, err := bufio.NewReader(os.Stdin).ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func guitar_trainer() {
+	frets := 22
+	chords_set := []string{"y", "yes"}
+	no_chords_set := []string{"n", "no"}
+	var chords bool
+
+	fmt.Println("Do you want to use chords?")
+	for {
+		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		str = strings.TrimSpace(str)
+		str = strings.ToLower(str)
+		if slices.Contains(chords_set, str) {
+			chords = true
+			break
+		} else if slices.Contains(no_chords_set, str) {
+			chords = false
+			break
+		} else {
+			fmt.Println("That's not a valid answer. Try 'y', 'yes', 'n' or 'no'")
+		}
+	}
+
+	fmt.Println("How many attacks should there be in a set?")
+	var number_of_attacks int
+	for {
+		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		str = strings.TrimSpace(str)
+
+		if number_of_attacks_buf, err := strconv.Atoi(str); err == nil {
+			number_of_attacks = number_of_attacks_buf
+			break
+		} else {
+			fmt.Println("That's not a valid number, try again")
+		}
+	}
+	sheet := make([][]int, number_of_attacks)
+	for i := range sheet {
+		sheet[i] = make([]int, 6)
+	}
+
+	fmt.Println("Press 'Enter' to get a new set")
+	if chords {
+		anchor_fret := int(rand.Int31n(int32(frets - 1)))
 		for {
 			for k := 0; k < number_of_attacks; k++ {
 				barre := rand_bool(1, 2)
-				third_finger := int(rand.Int31n(2))	// 1 if the chord'll uses extra finger,
+				third_finger := int(rand.Int31n(2))	// 1 if the chord'll use extra finger,
 				fourth_finger := int(rand.Int31n(2))// otherwise 0
 				chord_buffer := make([]int, 6)
 				for i := 0; i < len(chord_buffer); i++{
@@ -131,7 +273,7 @@ func guitar_trainer() {
 		}
 	} else {
 		for {
-			fret := int(rand.Int31n(23))
+			fret := int(rand.Int31n(int32(frets + 1)))
 			for k := 0; k < number_of_attacks; k++ {
 				buff_attack := make([]int, 6)
 				for i := 0; i < 6; i++ {
@@ -152,11 +294,11 @@ func guitar_trainer() {
 }
 
 func initiator() {
-	inst_options := []string{"f", "g"} // f - flute, g - guitar
+	inst_options := []string{"f", "g", "b"} // f - flute, g - guitar, b - bass
 	var str string
 
 	fmt.Println("Choose the instrument")
-	fmt.Println("f - flute, g - guitar")
+	fmt.Println("f - flute, g - guitar, b - bass")
 
 	for {
 		str_buf, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -177,6 +319,8 @@ func initiator() {
 		flute_trainer()
 	case "g":
 		guitar_trainer()
+	case "b":
+		bass_trainer()
 	}
 }
 
